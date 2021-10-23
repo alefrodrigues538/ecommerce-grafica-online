b185370561cdd63021ac14c9cd8a1a4e5c285aa8
alefrodrigues538 <alefrodrigues538@gmail.com>
2021-10-23 20:24:35 -0300

    initial commit

---
+++ components/Layout.jsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Script from 'next/script'
+
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+export default function Layout({ children }) {
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossOrigin="anonymous" />
+
+                <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
+
+            </Head>
+
+            <Navbar />
+            <main className="content">{children}</main>
+            <Footer />
+
+
+
+            <Script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+                crossOrigin="anonymous"></Script>
+        </>
+    )
+}
