03ed7427b4171d250e0ee82db10d32e0ff62be8a
alefrodrigues538 <alefrodrigues538@gmail.com>
2021-10-23 20:27:17 -0300

    initial commit

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
+import Script from 'next/script'
+
 export default function Layout({ children }) {
     return (
         <>
@@ -13,18 +15,16 @@ export default function Layout({ children }) {
 
                 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
 
-                <script
-                    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-                    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-                    crossOrigin="anonymous"></script>
             </Head>
 
             <Navbar />
             <main className="content">{children}</main>
             <Footer />
 
-
-
+            <Script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+                crossOrigin="anonymous"></Script>
         </>
     )
 }
